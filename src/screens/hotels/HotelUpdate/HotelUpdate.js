0370f02d056deb086ca17d935a79240cf203a28a
Ezequiel <oliverm316@gmail.com>
2022-03-24 17:32:03 +0100

    Update Hotels

--- src/screens/hotels/HotelUpdate/HotelUpdate.js
+++ src/screens/hotels/HotelUpdate/HotelUpdate.js
@@ -1,18 +1,54 @@
-import React, { useState } from "react";
-import baseAPI from "../../../axios/axiosConfig";
-import { updateHotel } from "../../../dev-data/hotel-data";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import baseAPI from "../../../axios/axiosConfig";
 
 const HotelUpdate = () => {
-  const location = useLocation();
-  const [updatedHotel, setUpdatedHotel] = useState({});
   const [originalHotel, setOriginalHotel] = useState({});
+  const [isInput, setIsInput] = useState({
+    name: false,
+    city: false,
+  });
+  const [updatedHotel, setUpdatedHotel] = useState({
+    name: "",
+    city: "",
+  });
+  const location = useLocation();
 
-  const handleHotelUpdate = async () => {
+  const getHotel = async () => {
+    try {
+      const recovered = await baseAPI.get(
+        `v1/hotels/${location.state.hotelId}`
+      );
+      setOriginalHotel(recovered.data.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getHotel();
+  }, []);
+
+  const setEditFieldStatus = (key, bool) => {
+    setIsInput({
+      ...isInput,
+      [key]: bool,
+    });
+  };
+
+  const handleUpdateHotel = (e) => {
+    setUpdatedHotel({
+      ...updatedHotel,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       const updated = await baseAPI.patch(
         `v1/hotels/${location.state.hotelId}`,
-        updateHotel
+        updatedHotel
       );
       setUpdatedHotel(updated.data.data.data);
     } catch (error) {
@@ -20,27 +56,56 @@ const HotelUpdate = () => {
     }
   };
 
-  const handleGetSingleHotel = async () => {
-    try {
-      const recovered = await baseAPI.get(
-        `v1/hotels/${location.state.hotelId}`
-      );
-      setOriginalHotel(recovered);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const renderOriginalHotel = (
+    <>
+      {isInput.name ? (
+        <input
+          type="text"
+          placeholder="New hotel name ..."
+          onBlur={() => setEditFieldStatus("name", false)}
+          name="name"
+          value={updatedHotel.name}
+          onChange={handleUpdateHotel}
+          autoFocus
+        />
+      ) : (
+        <li onClick={() => setEditFieldStatus("name", true)}>
+          {updatedHotel.name ? updatedHotel.name : originalHotel.name}
+        </li>
+      )}
+      {isInput.city ? (
+        <input
+          type="text"
+          placeholder="New city ..."
+          onBlur={() => setEditFieldStatus("city", false)}
+          name="city"
+          value={updatedHotel.city}
+          onChange={handleUpdateHotel}
+          autoFocus
+        />
+      ) : (
+        <li onClick={() => setEditFieldStatus("city", true)}>
+          {updatedHotel.city ? updatedHotel.city : originalHotel.city}
+        </li>
+      )}
+    </>
+  );
+
   return (
     <>
-      {JSON.stringify(originalHotel)}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h3>Original Hotel</h3>
+          {renderOriginalHotel}
+        </div>
+        <hr />
+        <button type="submit">Update and Save</button>
+      </form>
       <hr />
-      {JSON.stringify(updatedHotel)}
-      <button onClick={handleGetSingleHotel}>
-        {`Recover hotel - ${location.state.hotelName}`}
-      </button>
-      <button onClick={handleHotelUpdate}>
-        {`Amend hotel - ${location.state.hotelName}`}
-      </button>
+      <div>
+        <h3>Updated Hotel</h3>
+        {JSON.stringify(updatedHotel)}
+      </div>
     </>
   );
 };
