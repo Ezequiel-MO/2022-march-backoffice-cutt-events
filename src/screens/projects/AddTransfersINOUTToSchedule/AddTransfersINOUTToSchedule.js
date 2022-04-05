ac31f879a6a6804ec40fba868848420a07f14e8b
Ezequiel <oliverm316@gmail.com>
2022-04-05 16:01:02 +0200

    minor fix

--- src/screens/projects/AddTransfersINOUTToSchedule/AddTransfersINOUTToSchedule.js
+++ src/screens/projects/AddTransfersINOUTToSchedule/AddTransfersINOUTToSchedule.js
@@ -6,6 +6,8 @@ import {
   selectCurrentProject,
 } from "../../../redux/features/CurrentProjectSlice";
 import baseAPI from "../../../axios/axiosConfig";
+import { toast } from "react-toastify";
+import { toastOptions } from "../../../dev-data/toast";
 
 const AddTransfersINOUTToSchedule = () => {
   const location = useLocation();
@@ -47,29 +49,34 @@ const AddTransfersINOUTToSchedule = () => {
         event: transfer,
       })
     );
-    alert("Transfer added");
+    toast.success("Transfer added", toastOptions);
     navigate("/project/schedule");
   };
 
   const transferList = transfers.map((transfer) => (
-    <li key={transfer._id}>
-      {transfer.company} <span>{transfer.vehicleType}</span>
-      <span>{transfer.transfer_in_out}</span>
-      <button onClick={() => handleAddTransfer(transfer)}>
+    <tr key={transfer._id}>
+      <td>{transfer.company}</td>
+      <td>{transfer.vehicleType}</td>
+      <td>{transfer.vehicleCapacity}</td>
+      <td
+        onClick={() => handleAddTransfer(transfer)}
+        className="cursor-pointer"
+      >
         {location.state.timeOfEvent === "transfer_in" ? (
-          <p>Add Transfer In to the schedule</p>
+          <p>Add Transfer In</p>
         ) : (
-          <p>Add Transfer Out to the schedule</p>
+          <p>Add Transfer Out</p>
         )}
-      </button>
-    </li>
+      </td>
+    </tr>
   ));
 
   return (
     <>
+      <h1 className="text-2xl mb-4 indent-8">Transfer In/Out</h1>
       <form>
         {!currentProjectIsLive ? (
-          <div>
+          <div className="block relative w-64">
             <label htmlFor="cities">Filter by city:</label>
             <select
               name="cities"
@@ -82,13 +89,16 @@ const AddTransfersINOUTToSchedule = () => {
             </select>
           </div>
         ) : null}
-        <div>
+        <div className="block relative w-64">
           <label htmlFor="vehicleCapacity">Filter by Vehicle Size:</label>
           <select
             name="vehicleCapacity"
             id="vehicleCapacity"
+            className="block cursor-pointer w-full bg-white-100 border border-gray-400 hover:border-gray-50 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
             onChange={(e) => setVehicleCapacity(parseInt(e.target.value))}
           >
+            <option value="">Choose type of Vehicle</option>
+            <option value={3}>3-seat Mercedes</option>
             <option value={20}>Mini Bus</option>
             <option value={30}>30-seater Bus</option>
             <option value={50}>50-seater Bus</option>
@@ -96,7 +106,17 @@ const AddTransfersINOUTToSchedule = () => {
           </select>
         </div>
       </form>
-      <ul>{transferList}</ul>
+      <table className="table-auto col-span-3">
+        <thead className="bg-gray-50 border-b text-left">
+          <tr>
+            <th>Company</th>
+            <th>Vehicle Type</th>
+            <th>Vehicle Capacity</th>
+            <th>Add To Schedule</th>
+          </tr>
+        </thead>
+        <tbody className="text-white-50">{transferList}</tbody>
+      </table>
     </>
   );
 };
