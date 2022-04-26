d81d52b2b112f18a8fb7965c1de43d8ea2d47600
Ezequiel <oliverm316@gmail.com>
2022-04-26 16:56:40 +0200

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./screens/Dashboard/Dashboard";
 import EventList from "./screens/events/EventList/EventList";
