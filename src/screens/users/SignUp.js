4fdfcd3f63b00831c6460ed1b679a003091689c1
Ezequiel <oliverm316@gmail.com>
2022-06-03 12:08:48 +0200

    Sign Up component

--- src/screens/users/SignUp.js
+++ src/screens/users/SignUp.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import baseAPI from "../../axios/axiosConfig";
+
+import { toastOptions } from "../../dev-data/toast";
 
 const SignUp = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ([name, email, password, passwordConfirm].includes("")) {
+      toast.error("Please fill all the fields", toastOptions);
+      return;
+    }
+    if (password !== passwordConfirm) {
+      toast.error("Passwords do not match", toastOptions);
+      return;
+    }
+
+    if (password.length < 8 || passwordConfirm.length < 8) {
+      toast.error(
+        "Password must be between at least 8 characters",
+        toastOptions
+      );
+      return;
+    }
+
+    try {
+      const response = await baseAPI.post(`v1/users/signup`, {
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+      console.log("response", response);
+      toast.success("User created", toastOptions);
+      setName("");
+      setEmail("");
+      setPassword("");
+      setPasswordConfirm("");
+    } catch (error) {
+      toast.warn(error.data.message ?? "not able to sign up", toastOptions);
+    }
+  };
+
   return (
     <>
       <h1 className="font-black text-4xl capitalize">
         Create <span className="text-white-100">your Account</span>
       </h1>
-      <form className="my-10 bg-gray-50 rounded-lg px-10 py-5">
+      <form
+        className="my-10 bg-gray-50 rounded-lg px-10 py-5"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="name"
@@ -20,6 +69,8 @@ const SignUp = () => {
             id="name"
             placeholder="Your name"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-100"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -34,6 +85,8 @@ const SignUp = () => {
             id="email"
             placeholder="Register email"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-100"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -48,6 +101,8 @@ const SignUp = () => {
             id="password"
             placeholder="Register password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-100"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="my-5">
@@ -62,6 +117,8 @@ const SignUp = () => {
             id="confirmPassword"
             placeholder="Repeat your password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-100"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
           />
         </div>
         <input
